208881473418d59c7d8f00ad30434f060d9a7b85
awym.dev <awym.dev01@gmail.com>
2024-03-20 04:36:59 +0300

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom'
+import Navigation from './components/Navigation/Navigation';
 function App() {
   return (
     <div className="App">
-      hello react
+
+        <Routes>
+
+          <Route path='/' element={<Navigation/>}/>
+
+        </Routes>
+
     </div>
   );
 }
